7b7a5ffebff7f99bf2eda58bc29375b5b124a6a9
Molly Skelbye <molly.brandt09@gmail.com>
2023-05-15 16:53:02 +0200

    Time: 11 ms (25.99%), Space: 42.4 MB (29.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,13 +3,9 @@ class Solution {
     public boolean isPalindrome(int x) {
 
         char[] chars = ("" + x).toCharArray();
-        int rightIndex = 0; 
         int leftIndex = chars.length-1; 
         boolean isPalindrome = true;  
-        for ( int i = 0; i < chars.length; i++ ) {
-            if ( i >= (leftIndex-i)) {
-                break; 
-            }
+        for ( int i = 0; i < chars.length/2; i++ ) {
             if ( chars[leftIndex-i] != chars[i] ) {
                 isPalindrome = false; 
                 break; 
