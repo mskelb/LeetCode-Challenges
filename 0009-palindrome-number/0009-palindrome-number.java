a70d3881dd045f04b8ef52d0f8a86e18a5d579c8
Molly Skelbye <molly.brandt09@gmail.com>
2023-05-15 16:56:49 +0200

    Time: 10 ms (53.32%), Space: 42.2 MB (49.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,7 +3,6 @@ class Solution {
     public boolean isPalindrome(int x) {
 
         String str = String.valueOf(x);
-        int rightIndex = 0; 
         int leftIndex = str.length()-1; 
         boolean isPalindrome = true;  
         for ( int i = 0; i < str.length(); i++ ) {
