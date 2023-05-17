3c8985ebc0749b64cf6bd4e34e861418f75883cf
Molly Skelbye <molly.brandt09@gmail.com>
2023-05-17 09:41:33 +0200

    Time: 0 ms (100%), Space: 42.9 MB (46.66%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -5,9 +5,7 @@ class Solution {
         int[] nums2 = new int[n*2]; 
         int index = 0; 
         for( int i = 0; i < n; i++ ) {
-            // 0,2,4,6
             nums2[index] = nums[i]; 
-            //1,3,5 
             nums2[index+1] = nums[i+n];
             index = index + 2;  
                      
