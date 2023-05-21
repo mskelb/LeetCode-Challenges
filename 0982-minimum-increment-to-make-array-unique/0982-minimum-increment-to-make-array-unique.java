841d4d10804a802224323d5e0c63a332d1e35f3e
Molly Skelbye <molly.brandt09@gmail.com>
2023-05-21 09:00:11 +0200

    Time: 72 ms (18.51%), Space: 57.3 MB (8.26%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -7,18 +7,15 @@ class Solution {
         int max = 0; 
         int inc = 0; 
         HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>(); 
-        int i = 0; 
-        while ( i < nums.length  ) {
+        for ( int i = 0; i < nums.length; i++ ) {
             if ( !numbers.containsKey(nums[i]) ) {
                 numbers.put(nums[i], 0);
                 max = nums[i]; 
-                i++; 
             }
             else {
                 numbers.put(max + 1, 0);
                 max = max + 1; 
                 inc = inc + (max - nums[i]); 
-                i++; 
             } 
         }
         return inc; 
