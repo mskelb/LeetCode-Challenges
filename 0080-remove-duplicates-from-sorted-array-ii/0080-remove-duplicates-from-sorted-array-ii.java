e21479b917ea65c0b0bb775fe8d33fd1f2da7f5f
Molly Skelbye <molly.brandt09@gmail.com>
2023-06-21 10:50:34 +0200

    Time: 0 ms (100%), Space: 43.6 MB (65.31%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -2,13 +2,11 @@ class Solution {
 
     public int removeDuplicates(int[] nums) {
         int x = 2;
-
         for (int i = x; i < nums.length; i++) {
             if (nums[i] != nums[x - 2]) {
                 nums[x++] = nums[i];
             }
         }
-        System.gc();
         return x; 
     }
 }
