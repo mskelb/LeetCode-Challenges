8c760de4fa4a4b022c42006a7b74017719d07bbe
Molly Skelbye <molly.brandt09@gmail.com>
2023-06-26 13:30:01 +0200

    Time: 12 ms (36.89%), Space: 44 MB (54.4%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,13 +1,9 @@
 class Solution {
 
     public int minPartitions(String n) {
-        // Traverse the string
-        int max = 0; 
-        for (int i = 0; i < n.length(); i++) {
-            if (Character.getNumericValue(n.charAt(i)) > max ) {
-                max = Character.getNumericValue(n.charAt(i)); 
-            }
-        }
-    return max; 
+        char c[] = n.toCharArray();
+        Arrays.sort(c);
+        int lastIndex = n.length()-1;
+        return Character.getNumericValue(c[lastIndex]); 
     }
 }
