4212b03f7c1019b504e4cc4195e27fb81e90943b
Molly Skelbye <molly.brandt09@gmail.com>
2024-02-18 14:24:35 +0100

    Time: 2 ms (62.29%), Space: 41.5 MB (28.25%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    
+    public int sumOfUnique(int[] nums) {
+        
+        int sum = 0; 
+        // Sort the array: O(log n)
+        HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+        int length = nums.length; 
+        for (int i = 0; i < length; i++ ) {
+            if ( !numbers.containsKey(nums[i]) ) {
+                numbers.put(nums[i], 0);
+            } else {
+                numbers.replace(nums[i], 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : numbers.entrySet()) {
+            if ( entry.getValue() == 0 ) {
+                 sum = sum + entry.getKey(); 
+            }
+        }
+        
+     return sum;   
+    } 
+}
