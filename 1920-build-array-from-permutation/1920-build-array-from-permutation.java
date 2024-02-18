850c16f53d0983190abe211eeb78e1cc38b24e9a
Molly Skelbye <molly.brandt09@gmail.com>
2024-02-18 14:25:05 +0100

    Time: 3 ms (10.27%), Space: 42.1 MB (100%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int[] buildArray(int[] nums) {
+        
+        int[] ans = new int[nums.length]; 
+        for (int i = 0; i < nums.length; i++ ) {
+            ans[i] = nums[nums[i]]; 
+        }
+        System.gc();
+        return ans;
+
+
+        
+    }
+}
